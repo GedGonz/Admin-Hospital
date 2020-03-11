e36f85177be18e312065dafc62d28e723869102b
Gerald Gonzalez <gedgonz7@gmail.com>
2020-03-10 20:08:39 -0600

    creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+          path: 'dashboard',
+          component: DashboardComponent
+        },
+        {
+          path: 'login',
+          component: LoginComponent
+        },
+        {
+          path: 'register',
+          component: LoginComponent
+        },
+        {
+          path: 'progress',
+          component: ProgressComponent
+        },
+        {
+          path: 'graficas1',
+          component: Graficas1Component
+        },
+        {
+          path: '',
+          redirectTo: '/dashboard',
+          pathMatch: 'full'
+        },
+        {
+          path: '**',
+          component: NopagefoundComponent
+        }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
