836abbc4fad99fff794ce6eb3dda62a25995c49f
Gerald Gonzalez <gedgonz7@gmail.com>
2020-03-10 20:35:07 -0600

    creando rutas hijas, separando el login del dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,34 @@ import { LoginComponent } from './login/login.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
 
-const routes: Routes = [{
-          path: 'dashboard',
-          component: DashboardComponent
+const routes: Routes = [
+
+        {
+          path: '',
+          component: PagesComponent,
+          children: [
+                    {
+                      path: 'dashboard',
+                      component: DashboardComponent
+                    }, {
+                      path: 'progress',
+                      component: ProgressComponent
+                    },
+                    {
+                      path: 'graficas1',
+                      component: Graficas1Component
+                    },
+                    {
+                      path: '',
+                      redirectTo: '/dashboard',
+                      pathMatch: 'full'
+                    },
+          ]
         },
+
         {
           path: 'login',
           component: LoginComponent
@@ -19,19 +41,7 @@ const routes: Routes = [{
           path: 'register',
           component: LoginComponent
         },
-        {
-          path: 'progress',
-          component: ProgressComponent
-        },
-        {
-          path: 'graficas1',
-          component: Graficas1Component
-        },
-        {
-          path: '',
-          redirectTo: '/dashboard',
-          pathMatch: 'full'
-        },
+
         {
           path: '**',
           component: NopagefoundComponent
